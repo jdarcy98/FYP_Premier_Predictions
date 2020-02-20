ae030a331227f2bbddc861eaaae582ad847ba069
jdarcy98 <c16357773@mydit.ie>
2020-02-20 12:22:52 +0000

    Add files via upload

--- algorithmcreation.py
+++ algorithmcreation.py
@@ -1,53 +1,66 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import pymongo
+import seaborn as sns
+import csv
+from random import random,randint
+import math
+import numpy as np
+import sklearn
+import pickle
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-import tensorflow_datasets as tfds
-import tensorflow as tf
-from tensorflow import keras
+import pylab as plb
 import matplotlib.pyplot as plt
 
-def plot(historical, metrics):
-    plt.plot(historical.historical[metrics])
-    plt.plot(historical.historical['val_'+metrics], '')
-    plt.xlabel("Epochs")
-    plt.ylabel(metrics)
-    plt.legend([metrics, 'val_'+metric])
-    plt.show()
+plt.style.use('ggplot')
 
-dataf = pd.read_csv("2019_20fixtures.csv")
+dataf_full = pd.read_csv("2019_20fixtures.csv")
+dataf = dataf_full.drop(columns=['data__id', 'data__name', 'data__league_id', 'data__is_current_season', 'data__current_round_id',
+                                 'data__current_stage_id', 'data__fixtures__data__league_id', 'data__fixtures__data__stage_id',
+                                 'data__fixtures__data__weather_report', 'data__fixtures__data__weather_report__icon', 'data__fixtures__data__weather_report__temperature_celcius__unit',
+                                 'data__fixtures__data__weather_report__temperature__temp', 'data__fixtures__data__weather_report__temperature__unit',
+                                 'data__fixtures__data__weather_report__coordinates__lat', 'data__fixtures__data__weather_report__coordinates__lon',
+                                 'data__fixtures__data__weather_report__updated_at', 'data__fixtures__data__commentaries',
+                                 'data__fixtures__data__pitch', 'data__fixtures__data__details', 'data__fixtures__data__neutral_venue',
+                                 'data__fixtures__data__winning_odds_calculated', 'data__fixtures__data__scores__localteam_pen_score',
+                                 'data__fixtures__data__scores__visitorteam_pen_score', 'data__fixtures__data__scores__et_score',
+                                 'data__fixtures__data__scores__ps_score', 'data__fixtures__data__time__starting_at__date_time',
+                                 'data__fixtures__data__time__starting_at__date', 'data__fixtures__data__time__starting_at__timestamp',
+                                 'data__fixtures__data__time__starting_at__timezone', 'data__fixtures__data__time__minute',
+                                 'data__fixtures__data__time__second', 'data__fixtures__data__time__added_time',
+                                 'data__fixtures__data__time__extra_minute', 'data__fixtures__data__time__injury_time',
+                                 'data__fixtures__data__assistants__first_assistant_id', 'data__fixtures__data__assistants__second_assistant_id',
+                                 'data__fixtures__data__assistants__fourth_official_id', 'data__fixtures__data__leg', 'data__fixtures__data__colors',
+                                 'data__fixtures__data__colors__|', 'data__fixtures__data__colors__|__color',
+                                 'data__fixtures__data__colors__|__kit_colors', 'data__fixtures__data__deleted'])
 
-dataf['Date'] = pd.Categorical(dataf['Date'])
-dataf['Date'] = dataf.Date.cat.codes
 
-dataf['HomeTeam'] = pd.Categorical(dataf['HomeTeam'])
-dataf['HomeTeam'] = dataf.HomeTeam.cat.codes
+'''
+dataf['data__fixtures__data__weather_report__code'] = pd.Categorical(dataf['data__fixtures__data__weather_report__code'])
+dataf['data__fixtures__data__weather_report__code'] = dataf.data__fixtures__data__weather_report__code.cat.codes
 
-dataf['AwayTeam'] = pd.Categorical(dataf['AwayTeam'])
-dataf['AwayTeam'] = dataf.AwayTeam.cat.codes
+dataf['data__fixtures__data__weather_report__type'] = pd.Categorical(dataf['data__fixtures__data__weather_report__type'])
+dataf['data__fixtures__data__weather_report__type'] = dataf.data__fixtures__data__weather_report__type.cat.codes
 
-dataf['FTR'] = pd.Categorical(dataf['FTR'])
-dataf['FTR'] = dataf.FTR.cat.codes
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = pd.Categorical(dataf['data__fixtures__data__weather_report__temperature_celcius__temp'])
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = dataf.data__fixtures__data__weather_report__temperature_celcius__temp.cat.codes
 
-dataf['HTR'] = pd.Categorical(dataf['HTR'])
-dataf['HTR'] = dataf.HTR.cat.codes
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = pd.Categorical(dataf['data__fixtures__data__weather_report__temperature_celcius__temp'])
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = dataf.data__fixtures__data__weather_report__temperature_celcius__temp.cat.codes
 
-dataf['Referee'] = pd.Categorical(dataf['Referee'])
-dataf['Referee'] = dataf.Referee.cat.codes
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = pd.Categorical(dataf['data__fixtures__data__weather_report__temperature_celcius__temp'])
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = dataf.data__fixtures__data__weather_report__temperature_celcius__temp.cat.codes
 
-targets = dataf.pop('FTR', '')
-irrelevant = dataf.pop('Div', 'HTR')
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = pd.Categorical(dataf['data__fixtures__data__weather_report__temperature_celcius__temp'])
+dataf['data__fixtures__data__weather_report__temperature_celcius__temp'] = dataf.data__fixtures__data__weather_report__temperature_celcius__temp.cat.codes
+'''
 
-full_dataset = tf.data.Dataset.from_tensor_slices((dataf.values, target.values))
 
-#for feat, targ in full_dataset.take(10):
-#    print('Features: {}, Target: {}'.format(feat,targ))
-
-#print(tf.constant(dataf['Referee']))
-
-training_set = full_dataset.shuffle(len(dataf)).batch(1)
-
-def create_model():
-    model = tf.keras.Sequential([
-
-    ])
-#print(full_dataset)
+print(dataf_prep)

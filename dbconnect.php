65c4c57cfab71aa62bd61b6a9bd1d22b0452fc6b
jdarcy98 <c16357773@mydit.ie>
2020-03-16 12:23:16 +0000

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    //This php script opens the database, it is included at the top of every page that needs to access the database.
-
-    $con = mysqli_connect("localhost", "root", "", "premierpredictions");
-
-?>

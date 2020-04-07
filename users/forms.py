fca0aa6110cd0cc9c5e802b1df35ee7d4c5b348b
jdarcy98 <c16357773@mydit.ie>
2020-04-07 19:17:48 +0100

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms import modelform_factory 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from league.models import UserPredictions
 
 class UserRegistrationForm(UserCreationForm):
 	email = forms.EmailField()
@@ -21,3 +23,9 @@ class prof_update_form(forms.ModelForm):
 	class Meta:
 		model = Profile
 		fields = ['pic']
+
+
+'''class PredictForm(forms.ModelForm):
+	class Meta:
+		model = UserPredictions
+		fields = ['home_score', 'away_score']'''

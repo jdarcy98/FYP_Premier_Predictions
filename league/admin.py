d7d2d0d1c23b5c5e4a6163d6e2684f54e7b3dffc
jdarcy98 <c16357773@mydit.ie>
2020-04-10 19:36:26 +0100

    Add files via upload

--- league/admin.py
+++ league/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Table
-from .models import FixtureInfo
+from .models import *
 
 admin.site.register(Table)
 admin.site.register(FixtureInfo)
+admin.site.register(UserPredictions)
+admin.site.register(FinalScores)
--- league/apps.py
+++ league/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class LeagueConfig(AppConfig):
-    name = 'league'
+	name = 'league'
+
+	def ready(self):
+		import league.signals
